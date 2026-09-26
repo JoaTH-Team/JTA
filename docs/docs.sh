160f8ad277b8c31e139491ff5b68819686563cc9
Joalor64 <rhgarcon@gmail.com>
2026-09-25 22:27:36 -0400

    Update docs.sh

--- docs/docs.sh
+++ docs/docs.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
-
 haxe docs/docs.hxml
-haxelib run dox -i docs -o pages --title "Journey Through Aubekhia Documentation" -D source-path https://github.com/JoaTH-Team/JTA/tree/main/source
+haxelib run dox -i docs -o pages --title "Journey Through Aubekhia Documentation" -D source-path https://github.com/JoaTH-Team/JTA/tree/main/source -D logo "https://raw.githubusercontent.com/JoaTH-Team/JTA/main/icon.png"
